7b58362fa4f18e191fbb028a081461fd11d12c41
Sławomir Mucha <slawomir.mucha@skygate.pl>
2016-06-02 01:28:01 +0200

    Add basic client functions structure

--- src/client.c
+++ src/client.c
@@ -1,17 +1,51 @@
-#include <stdio.h>
+#include <pthread.h>
+#include <unistd.h>
 #include "message.h"
 #include "socket_utils.h"
+#define SYNC_TIME 10
+#define SERVER_BOX_FILENAME ".server_box"
+#define LOCAL_BOX_FILENAME ".box"
 
 
 
-void listen()
+int file_exists(char* filename)
+{
+    return access(filename, F_OK);
+}
+
+
+void* pull_changes(void *parameters)
+{
+    while(1) {
+
+    }
+
+    return NULL;
+}
+
+
+void* track_directory(void *parameters)
+{
+    while(1) {
+
+        sleep(SYNC_TIME);
+    }
+
+
+    return NULL;
+}
+
+
+void receive_server_box(int socket_fd)
 {
 
 }
 
 
-void track()
+void initialize_local_box()
 {
+    if(file_exists(LOCAL_BOX_FILENAME)) return;
+
 
 }
 
@@ -19,7 +53,28 @@ void track()
 
 int main(int argc, char *argv[])
 {
+    pthread_t tracker_id, listener_id;
+    int socket_fd;
 
+    socket_fd = get_client_socket(argv[1], atoi(argv[2]));
+
+    receive_server_box(socket_fd);
+
+    initialize_local_box();
+
+
+    if(pthread_create(&tracker_id, NULL, &track_directory, NULL) < 0) {
+        perror("pthread_create failed");
+        return EXIT_FAILURE;
+    }
+
+    if(pthread_create(&listener_id, NULL, &pull_changes, NULL) < 0) {
+        perror("pthread_create failed");
+        return EXIT_FAILURE;
+    }
+
+    if(pthread_join(tracker_id, NULL) < 0) { perror("pthread_join failed"); return EXIT_FAILURE; }
+    if(pthread_join(listener_id, NULL) < 0) { perror("pthread_join failed"); return EXIT_FAILURE; }
 
     return EXIT_SUCCESS;
 }
