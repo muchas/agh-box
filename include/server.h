70130378df01c0ebe6821b0d9c0e58d9e4a0aeba
Michał Sadowski <sadowskimichal95@gmail.com>
2016-06-02 02:34:57 +0200

    base server functions

--- include/server.h
+++ include/server.h
@@ -1,4 +1,14 @@
 #ifndef server_h
 #define server_h
 
+#define MAX_CLIENT_NUM 64
+
+typedef struct {
+    int highest_fd;
+    fd_set server_fd_set;
+    int socket;
+    int client_array[MAX_CLIENT_NUM];
+    int client_num;
+} server_t;
+
 #endif
--- src/server.c
+++ src/server.c
@@ -1,5 +1,5 @@
 #include "server.h"
-#include "message.h"
+#include "communication_utils.h"
 #include "socket_utils.h"
 
 int main(){
@@ -14,3 +14,50 @@ int main(){
 }
 
 
+void run(server_t server){
+    int result, fd;
+    fd_set read_set;
+
+    while(1){
+        read_set = server.server_fd_set;
+        result = select(highest_fd+1, &read_set, NULL, NULL, NULL);
+        if(result<0){
+            perror("Select error");
+            exit(EXIT_FAILURE);
+        }
+
+        for(fd=0; fd<=highest_fd; fd++){
+            if(FD_ISSET(fd, &read_set))
+            {
+                if(fd==socket){
+                    register_client(&server);
+                }
+                else{
+
+                }
+            }
+        }
+
+    }
+
+}
+
+server_t get_server(int socket){
+    server_t server;
+    server.socket = socket;
+    FD_ZERO(&server.server_fd_set);
+    FD_SET(socket, &server.server_fd_set);
+    server.highest_fd = socket;
+    server.client_num=0;
+
+    return server;
+}
+
+void register_client(server_t* server){
+    int client_fd;
+    client_fd = accept(socket_fd, NULL, 0);
+    if(client_fd<0){
+        perror("accept error");
+        exit(EXIT_FAILURE);
+    }
+}
