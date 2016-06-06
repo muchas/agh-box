d93a78f9ee1a9d6aecf3939f0134f2d2def6f48e
Sławomir Mucha <slawomir.mucha@skygate.pl>
2016-06-06 13:53:50 +0200

    Add box interface functions, stack utils and local files listing

--- include/communication_utils.h
+++ include/communication_utils.h
@@ -11,22 +11,26 @@ typedef enum {
     SERVER_BOX,
     FILE_REQUEST,
     SERVER_FILE,
-    CLIENT_FILE
+    CLIENT_FILE,
+    FILE_REMOVAL
 } message_type_t;
 
 typedef struct {
     message_type_t message_type;
     size_t size;
+    time_t modification_time;
     char name[MAX_PATHLEN];
 } message_info_t;
 
-
-typedef struct {
+typedef struct BoxEntry {
     time_t global_timestamp;
     time_t local_timestamp;
     int is_directory;
     char path[MAX_PATHLEN];
+
+    struct BoxEntry* next;
 } box_entry_t;
+
 
 void send_file(int socket, const char* path, message_type_t type);
 message_info_t receive_message_info(int socket);
